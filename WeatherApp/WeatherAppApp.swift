33a6dd403c9d0190843c380454f3412e015970de
Casiebelle Le <aprilcasie@gmail.com>
2025-05-11 00:32:33 +1000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Casiebelle Le on 11/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
